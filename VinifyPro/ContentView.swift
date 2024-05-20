c3687427007de97beff1eeccb7ac37a6b420e498
DanutPopa <popa.ion.danut@gmail.com>
2024-05-20 14:25:40 +0300

    Making the UI for the login screen.

--- VinifyPro/ContentView.swift
+++ VinifyPro/ContentView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State private var email: String = ""
+  @State private var password: String = ""
+
+  var body: some View {
+    VStack(alignment: .leading) {
+
+      VStack {
+        TextField("Email", text: $email)
+        TextField("Password", text: $password)
+      }
+      .textFieldStyle(.roundedBorder)
+
+      Spacer()
+        .frame(maxHeight: 25)
+
+      HStack {
+        Spacer()
+        Button {
+          
+        } label: {
+          Text("Login")
+            .font(.title3)
+            .foregroundStyle(.white)
         }
-        .padding()
+        .buttonStyle(.borderedProminent)
+        Spacer()
+      }
+      .background(
+        RoundedRectangle(cornerRadius: 10)
+          .fill(Color.blue)
+      )
     }
+    .padding(.horizontal)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
+

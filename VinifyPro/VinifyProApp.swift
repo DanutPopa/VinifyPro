2d24deccb158e4437f669223469a433ddcd78cd5
DanutPopa <popa.ion.danut@gmail.com>
2024-05-20 12:45:48 +0300

    Initial Commit

---
+++ VinifyPro/VinifyProApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinifyProApp.swift
+//  VinifyPro
+//
+//  Created by Danut Popa on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct VinifyProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
